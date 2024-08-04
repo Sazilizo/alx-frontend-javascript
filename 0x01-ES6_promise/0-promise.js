7362d042acd63cff84c08037ea26c051f2fae7c5
unknown <lizosazi@gmail.com>
2024-08-04 19:41:49 +0200

    initial files

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,9 @@
+function getFullResponseFromAPI(success) {
+    const promise = new Promise((resolve, reject) => {
+        if (success) resolve({ status: 200, body: 'Success' })
+        else reject(Error('The fake API is not working currently'));
+    });
+    return promise
+}
+  
+export default getFullResponseFromAPI;
