2309ff24e9cf50c89091eaa97b46ce16b33d4905
unknown <lizosazi@gmail.com>
2024-08-04 20:04:38 +0200

    fixed errors to 0-promise.js and 1-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,9 +1,8 @@
 function getFullResponseFromAPI(success) {
   const promise = new Promise((resolve, reject) => {
-    if (success) resolve({ status: 200, body: 'Success' })
-    else reject(Error('The fake API is not working currently'));
+    if (success) resolve({ status: 200, body: 'Success' });
+    reject(Error('The fake API is not working currently'));
   });
   return promise;
 }
-  
 export default getFullResponseFromAPI;
