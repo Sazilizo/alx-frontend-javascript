61d01151546c97d4090623d9a50bf1f0e4f26a40
unknown <lizosazi@gmail.com>
2024-08-04 23:11:21 +0200

    added 8-clean-set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,13 +1,10 @@
 export default function cleanSet(set, startString) {
-  if (!startString || !startString.length) {
-    return '';
-  }
-  let string = '';
-  for (const i of set) {
-    if (i.startsWith(startString)) {
-      string += string.length === 0 ? i.replace(startString, '')
-        : i.replace(startString, '-');
-    }
-  }
-  return string;
+  if (!startString || !startString.length || typeof startString !== 'string') return '';
+
+  let finalString = '';
+  set.forEach((element) => {
+    if (element && element.startsWith(startString)) finalString += `${element.slice(startString.length)}-`;
+  });
+
+  return finalString.slice(0, finalString.length - 1);
 }
